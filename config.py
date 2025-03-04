c098384da47a805c04ca057e147932a5d4dea38c
ミ★ 𝕰𝖓𝖌𝖎𝖓𝖊𝖊𝖗𝖘 𝕭𝖆𝖇𝖚 ★彡 <jaibajarangclasses@gmail.com>
2025-03-04 22:27:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 28228907
-api_hash = "3848da16c807890cabc8d1395248c9cf3"
-bot_token = "6945259945:AAEG0hlngK-ORA4VrwZ6GxwybN8ei4H80jDwI"
-auth_users = "6127558921"
+api_id = "21705536"
+api_hash = "c5bb241f6e3ecf33fe68a444e288de2d"
+bot_token = "7979826252:AAFihIFHcUhlpN5b3RBgpCGm-iqSKdTsndI"
+auth_users = "5957208798"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6127558921"
+osowner_users = "5957208798"
 owner_users = [int(num) for num in osowner_users.split(",")]
